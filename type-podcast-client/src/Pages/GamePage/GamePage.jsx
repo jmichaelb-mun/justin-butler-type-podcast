d10c9baa4701623c5746c9cdfe551191ceabb0b1
Justin Butler <jmichaelb@mun.ca>
2024-09-19 23:17:38 -0230

    added connection to receive the players current type from Unity and made the Gameplay window larger

--- type-podcast-client/src/Pages/GamePage/GamePage.jsx
+++ type-podcast-client/src/Pages/GamePage/GamePage.jsx
@@ -2,18 +2,27 @@
 import React from 'react'
 import { Unity, useUnityContext } from "react-unity-webgl";
 import { useState, useCallback, useEffect } from 'react';
+import './GamePage.scss'
 import axios from "axios";
 
 function GamePage() {
     const [isGameOver, setIsGameOver] = useState(false);
     const [score, setScore] = useState();
+    const [type, setType] = useState();
+    const [isType, setIsType] = useState(false);
 
     const { unityProvider, addEventListener, removeEventListener } = useUnityContext({
-        loaderUrl: "build/builds.loader.js",
-        dataUrl: "build/builds.data",
-        frameworkUrl: "build/builds.framework.js",
-        codeUrl: "build/builds.wasm",
+        loaderUrl: "build/build.loader.js",
+        dataUrl: "build/build.data",
+        frameworkUrl: "build/build.framework.js",
+        codeUrl: "build/build.wasm",
     });
+
+    const handleSendType = useCallback((type) => {
+        console.log(type);
+        setIsType(true);
+        setType(type);
+    }, []);
 
     const handleGameOver = useCallback((score) => {
         setIsGameOver(true);
@@ -26,13 +35,23 @@ function GamePage() {
             removeEventListener("GameOver", handleGameOver);
         };
     }, [addEventListener, removeEventListener, handleGameOver]);
+    useEffect(() => {
+        addEventListener("SendType", handleSendType);
+        return () => {
+            removeEventListener("SendType", handleSendType);
+        };
+    }, [addEventListener, removeEventListener, handleSendType]);
 
     return (
         <>
+            {isType === true && (
+                <p>{`Player Type: ${type}`}</p>
+            )}
             <Unity unityProvider={unityProvider} />
             {isGameOver === true && (
                 <p>{`Game Over! You've scored ${score} points.`}</p>
             )}
+
         </>
     )
 }

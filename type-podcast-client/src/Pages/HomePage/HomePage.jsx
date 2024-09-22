5548ed308ef1b24705831a8b8013dcce551cadd0
Justin Butler <jmichaelb@mun.ca>
2024-09-22 20:50:45 -0230

    Fixed styling with the home page that got adjusted by the header

--- type-podcast-client/src/Pages/HomePage/HomePage.jsx
+++ type-podcast-client/src/Pages/HomePage/HomePage.jsx
@@ -2,11 +2,14 @@ import { Link } from "react-router-dom";
 import "./HomePage.scss"
 import Header from "../../components/Header/Header";
 
-export default function HomePage(){
-    return(
+export default function HomePage() {
+    return (
         <>
-        <Header/>
-        <Link to={"/play"} className="play-link">Play</Link>
+            <Header />
+            <div className="menu-cont">
+                <Link to={"/play"} className="play-link">PLAY</Link>
+                <Link to={"/hall-of-fame"} className="hof-link">HALL OF FAME</Link>
+            </div>
         </>
     )
 }

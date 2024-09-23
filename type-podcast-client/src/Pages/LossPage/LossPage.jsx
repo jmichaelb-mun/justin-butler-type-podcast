c896e80c50032bbd9307021fab58bf0408a15971
Justin Butler <jmichaelb@mun.ca>
2024-09-22 23:21:47 -0230

    Loss Page Updates to more closely match the styling of the rest of the website, small fix to completion modal on mobile

--- type-podcast-client/src/Pages/LossPage/LossPage.jsx
+++ type-podcast-client/src/Pages/LossPage/LossPage.jsx
@@ -1,20 +1,26 @@
 import { TypeAnimation } from 'react-type-animation';
 import "./LossPage.scss"
 import { useNavigate } from 'react-router-dom';
+import Header from '../../components/Header/Header';
 
 export default function LossPage() {
     const navigate = useNavigate();
-    const handleCLick = ((e)=>{
+    const handleCLick = ((e) => {
         e.preventDefault();
         navigate('/', { replace: true });
     })
     return (
-        <div className='loss=page' onClick={handleCLick}>
-            <TypeAnimation
-                sequence={["You are out of usable Pokemon and scurry back to the Pokemon Center, protecting your exhausted Pokemon from any further harm..."]}
-                className='typing-text'
-                speed={30}
-            />
+        <div>
+            <Header />
+            <div className='loss-page'>
+                <div className='loss-page__border-box' onClick={handleCLick}>
+                    <TypeAnimation
+                        sequence={["You are out of usable Pokemon and scurry back to the Pokemon Center, protecting your exhausted Pokemon from any further harm..."]}
+                        className='typing-text'
+                        speed={50}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
